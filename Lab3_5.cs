32f8c16aac15d35d24a7615c4b68933768dd3a28
villjr <61257261+villjr@users.noreply.github.com>
2020-02-20 19:06:52 -0600

    Add files via upload

---
+++ Lab3_5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWord = "crap";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
+        }
+    }
+}
